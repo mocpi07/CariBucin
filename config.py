6ba60fba69e38604e40b8f0764651c5cd14278e2
mocpi07 <96298243+mocpi07@users.noreply.github.com>
2022-02-02 12:43:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os 
 
 TOKEN = os.environ.get("TOKEN", "")
-OWNER = os.environ.get("OWNER", "AmiFutami")
-GROUP = os.environ.get("GROUP", "safothebot")
-CHANNEL = os.environ.get("CHANNEL", "AsmSafone")
-BOT_NAME = os.environ.get("BOT_NAME", "Anonymous ChatBot")
+OWNER = os.environ.get("OWNER", "xycye")
+GROUP = os.environ.get("GROUP", "GabutIsUsOfficial")
+CHANNEL = os.environ.get("CHANNEL", "Userbot7STAR")
+BOT_NAME = os.environ.get("BOT_NAME", "Cari Bucinbot")
